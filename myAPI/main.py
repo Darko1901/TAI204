e5a00fd5fb67e9c1a7b2be66be4203bd785ed406
Darko1901 <123046770@upq.edu.mx>
2026-02-11 15:38:33 -0600

    Hicimos todo lo referente a parametros, endpoints y swagger

--- myAPI/main.py
+++ myAPI/main.py
@@ -1,21 +1,57 @@
 # importaciones
 from fastapi import FastAPI
 import asyncio 
+from typing import Optional
 
 # instancias del servidor
 
-app = FastAPI()
+app = FastAPI(
+    title = "Mi Primer API",
+    description= "Ricardo Méndez",
+    version= "1.0"
+)
+
+# TABLA FICTICIA
+usuarios = [
+    {"id":1, "nombre":"Diego", "edad":21},
+    {"id":2, "nombre":"Diego2", "edad":22},
+    {"id":3, "nombre":"Diego3", "edad":23}
+]
 
 # endpoints
-@app.get("/")
+@app.get("/", tags=['Inicio'])
 # SI vemos una async es porque habrá una peticion, una peticion siempre tiene que ir acompañada de un await
 async def bienvenido():
     return {"mensaje":"Bienvenido a FastAPI"} # La clave es "mensaje" y el valor es "Bienvenido a..."
 
-@app.get("/HolaMundo")
+@app.get("/HolaMundo", tags=['Asincronía'])
 async def hola():
     await asyncio.sleep(5) # peticion, consultaBD, Archivo
     return {
         "mensaje": "Hola Mundo FastAPI",
         "status": "200"
         }
+
+
+# PARAMETRO OBLIGATORIO
+@app.get("/v1/usuario/{id}", tags=['Paramétro Obligatorio']) # Con las llaves especificamos que el parametro es obligatorio
+async def consultaUno(id:int):
+    return {"mensaje":"Usuario encontrado",
+            "usuario:":id,
+            "status": "200"}
+
+# PARAMETRO OPCIONAL
+# requisitos: el endpoint debe de tener otro nombre porque son get, no necesita las llaves y la funcion debe de tener su propio nombre
+@app.get("/v1/usuarios/", tags=['Paramétro Opcional']) # Con las llaves especificamos que el parametro es obligatorio
+async def consultarTodos(id:Optional[int]=None):
+    if id is not None:
+        for usuarioK in usuarios:
+            if usuarioK["id"] == id:
+                return{"mensaje":"Usuario encontrado",
+                       "usuario:": usuarioK,
+                       "status": "200"}
+        return{"mensaje": "usuario no encontrado",
+               "status":"200"}
+    else:
+        return{"mensaje": "no se insertó ningún usuario para buscar",
+               "status":"200"}
